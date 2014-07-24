4d98a1aedeb86d39447b644bb0623e56824d63fe
Neeraj Singh <neerajsinghsonu@gmail.com>
2014-07-24 19:52:28 +0530

    update config file
    update config file

--- config.php
+++ config.php
@@ -9,8 +9,8 @@ session_id() ? session_start() : false;
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', '4Nog92IipVjPrf30irrdzTCKq');
-define('CONSUMER_SECRET', 'QTOn0N76VfcBkSp4iWbbB7h5vhmEOWSRGepChYF3eEqc0Y2biO');
-define('ACCESS_TOKEN', '2300999882-OYP0gujAexkN0kt8uFFJjvflsGa41incYTAcgus');
-define('ACCESS_TOKEN_SECRET', 'iHQxylJm6FRdK7z1w0Prh53ot6vmEtO6ouEVqLqV3aRES');
+define('CONSUMER_KEY', 'APP-KEY');
+define('CONSUMER_SECRET', 'APP-SECRET');
+define('ACCESS_TOKEN', 'ACCESS-TOKEN');
+define('ACCESS_TOKEN_SECRET', 'TOKEN-SECRET');
 define('OAUTH_CALLBACK', 'http://git.local.com/TwitterHashTagGallery/callback.php');
